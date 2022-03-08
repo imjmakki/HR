fd12699db3a193f85d663cab5e0ae5c3df0fcbe7
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-08 11:58:12 +0300

    adding the Data Access Object

---
+++ src/main/java/app/hr/hure/DAO/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package app.hr.hure.DAO;
+
+import app.hr.hure.Domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeDAO extends JpaRepository<Employee, Long> {
+}
