ae326abd97c95425ff416770fe46c92635374a91
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-08 13:10:02 +0300

    adding the api configuration

--- src/main/java/app/hr/hure/API/EmpAPI.java
+++ src/main/java/app/hr/hure/API/EmpAPI.java
@@ -1,9 +1,17 @@
 package app.hr.hure.API;
 
+import app.hr.hure.Domain.Employee;
 import app.hr.hure.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("employee")
@@ -14,5 +22,17 @@ public class EmpAPI {
     @Autowired
     public EmpAPI(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Employee>> listEmployees () {
+        List<Employee> employees = employeeService.allEmployees();
+        return new ResponseEntity<>(employees, OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id) {
+        Employee employee = employeeService.getEmployee(id);
+        return new ResponseEntity<>(employee, OK);
     }
 }
