cd9b1fd6f4ac623c676620bdd8f5fa77cd86d2bc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-08 11:28:48 +0300

    start adding the backend

---
+++ src/main/java/app/hr/hure/HureApplication.java
@@ -0,0 +1,13 @@
+package app.hr.hure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HureApplication.class, args);
+    }
+
+}
