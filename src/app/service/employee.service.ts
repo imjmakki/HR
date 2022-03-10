11a73a2bb27a0fb6777c1754978f781b3374a5aa
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-10 18:46:47 +0300

    start adding first api route

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,8 @@ import {HttpClient} from "@angular/common/http";
 export class EmployeeService {
 
   constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<any>{
+
+  }
 }
