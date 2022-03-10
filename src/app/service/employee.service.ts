c9b0c54418efd5616dcfbafc85d8db6a052751b2
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-10 19:07:52 +0300

    put method

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -18,4 +18,8 @@ export class EmployeeService {
   public addEmployee(employee: Employee): Observable<Employee>{
     return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
   }
+
+  public updateEmployee(employee: Employee): Observable<Employee>{
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/edit`, employee);
+  }
 }
